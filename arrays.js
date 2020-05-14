38bd57a6454facca9f14bd1ce5167302dea2ad4f
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-05-14 18:17:36 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,37 @@
+// var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+//
+// function addElementToBeginningOfArray(array, element){
+//   array.unshift(element)
+//   return array
+// }
+//
+// function destructivelyAddElementToBeginningOfArray(array, element){
+//   array.unshift(element)
+//   return array
+// }
+// //
+// function addElementToEndOfArray(array, element){
+//   array.push(element)
+//   return array
+// }
+//
+//
+// function destructivelyAddElementToEndOfArray(array, element) {
+//   array.push(element)
+//   reurn array
+// }
+
+function destructivelyRemoveElementFromBeginningOfArray (array){
+  array.shift(0)
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  array.slice(1)
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
